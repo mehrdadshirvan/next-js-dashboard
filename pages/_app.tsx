0356fc0c2d088223c847c9fa06586b4e866bb76f
Mehrdadshirvan <mehrdadshirvan@gmail.com>
2023-02-26 08:18:19 +0330

    login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'tailwindcss/tailwind.css';
+import 'tailwindcss/base.css';
+import 'tailwindcss/components.css';
+import 'tailwindcss/utilities.css';
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
