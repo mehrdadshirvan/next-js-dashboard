c6e37b2749feff4d4a17d878850cdf5c139e1007
mehrdadshirvan <mysmartapply.it@gmail.com>
2023-03-05 21:58:58 +0330

    dashboard

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -18,10 +18,12 @@ function login() {
             <div className="container">
                 <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
                     <form>
-                        <input type="text"
-                               name="mobile"
-                               id="mobile"/>
-                        <input type='text' name='password' id='password'/>
+                        <input id="email-address" name="email" type="email" autoComplete="email" required
+                               className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                               placeholder="Enter your email" />
+                        <input id="email-address" name="email" type="email" autoComplete="email" required
+                               className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                               placeholder="Enter your email" />
                         <button type='button' onClick={LoginRequest}>
                             login
                         </button>
