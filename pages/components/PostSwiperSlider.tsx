d5b1977a006ab6097fac62859ea61663bdca3489
mehrdadshirvan <mysmartapply.it@gmail.com>
2023-03-06 21:45:42 +0330

    swiper slider

---
+++ pages/components/PostSwiperSlider.tsx
@@ -0,0 +1,53 @@
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+import PostSwiperSliderCard from "./postSwiperSliderCard";
+import {Navigation, Pagination, Scrollbar, A11y} from 'swiper';
+import {BsBookmarks} from "react-icons/bs";
+
+// @ts-ignore
+const PostSwiperSlider = ({posts, title, slidesPerView = 5}) => {
+    return (
+        <div className={`mb-4`}>
+            <div className={`row mb-3`}>
+                <h3 className={`font-18 font-bold`}>
+                    <BsBookmarks className={`inline-block text-green-800`}/>
+                    {title}
+                </h3>
+            </div>
+            <Swiper spaceBetween={20}
+                    slidesPerView={slidesPerView}
+                    modules={[Navigation, Pagination, Scrollbar, A11y]}
+                    navigation
+                    breakpoints={{
+                        // when window width is >= 320px
+                        320: {
+                            slidesPerView: 2,
+                            spaceBetween: 20
+                        },
+                        // when window width is >= 480px
+                        480: {
+                            slidesPerView: 2,
+                            spaceBetween: 30
+                        },
+                        // when window width is >= 640px
+                        640: {
+                            slidesPerView: 3,
+                            spaceBetween: 40
+                        },
+                        // when window width is >= 980
+                        980: {
+                            slidesPerView: 5,
+                            spaceBetween: 10
+                        }
+                    }}
+                    pagination={{clickable: true}}
+            >
+                {/* eslint-disable-next-line react/jsx-key */}
+                {posts.map((post: any) => <SwiperSlide><PostSwiperSliderCard post={post}/></SwiperSlide>)}
+            </Swiper>
+
+        </div>
+    );
+};
+
+export default PostSwiperSlider;
---
+++ pages/components/postSwiperSliderCard.tsx
@@ -0,0 +1,48 @@
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+import Link from 'next/link';
+import Image from 'next/image';
+import {TiChevronRight} from "react-icons/ti";
+
+// @ts-ignore
+const PostSwiperSliderCard = ({post}) => {
+    // @ts-ignore
+    return (
+
+        <div className={`card border p-2 rounded shadow`}>
+            <div>
+                <div className={`p-2`}>
+                    <Link href={`post/${post.id}`}>
+                        <Image
+                            src={'https://www.mehrdadshirvan.ir/public/images/mehrdad-shirvan-1.jpg'}
+                            alt="Picture of the author"
+                            width={500}
+                            height={500}
+                            quality={50}
+                        />
+                    </Link>
+                </div>
+            </div>
+            <div className={`p-2`}>
+                <p style={{minHeight: '51px'}} className={`mb-4`}>
+                    <Link href={`/post/${post.id}`}
+                          className={`text-decoration-none text-secondary font-15 text-justify`}>
+                        {post.title.substring(0, 40)}...
+                    </Link>
+                </p>
+                <p className={`mb-0 text-start `}>
+                    <Link href={`/post/${post.id}`}
+                          className={`bg-green-300 hover:bg-green-700 text-white font-xs font-bold py-2 px-2 rounded transition ease-in-out `}>
+                        <span>
+                        Show more
+                            <TiChevronRight className={`inline-block`}/>
+                        </span>
+                    </Link>
+                </p>
+            </div>
+        </div>
+
+    );
+};
+
+export default PostSwiperSliderCard;
