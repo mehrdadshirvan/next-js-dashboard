a19d9cba6acc31df5f90b631eaa75a28e008717e
mehrdadshirvan <mysmartapply.it@gmail.com>
2023-03-06 20:23:07 +0330

    login

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,18 +1,40 @@
 import Image from "next/image";
 import Head from "next/head";
 import {useState} from "react";
+import {baseUrl} from "@/pages/helper";
 
 function login() {
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [name, setName] = useState(0);
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [password, setPassword] = useState(0);
-    function LoginRequest(){
+    const [form, setForm] = useState({
+        'mobile': "09228417016",
+        'password': "9092301030"
+    });
 
-        console.log(1)
+    const SetData = (event: { target: { name: any; value: any; }; }) => {
+        // console.log(event.target.name, event.target.value)
+        setForm({
+            ...form,
+            [event.target.name]: event.target.value,
+        })
     }
-    // @ts-ignore
-    // @ts-ignore
+
+    const submitHandler = async (event: { preventDefault: () => void; }) => {
+        event.preventDefault();
+        // let url = baseUrl('login');
+        let res = await fetch(baseUrl('login'), {
+            method: "POST",
+            body: JSON.stringify(form),
+            redirect: 'follow',
+            headers: {'Accept':'application/json','Content-Type':'application/json'},
+        });
+        res = await res.json();
+        // if(res.statusCode === 200){
+        console.log(res)
+        //     localStorage.setItem('_ut', '1')
+        // }else{
+        //     localStorage.removeItem('_ut');
+        // }
+    }
     return (
         <>
             <Head>
@@ -22,14 +44,28 @@ function login() {
             </Head>
             <div className="container">
                 <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-                    <form>
-                        <input id="email-address" name="email" type="email" autoComplete="email" required
+                    <form onSubmit={submitHandler}>
+                        <input id="mobile"
+                               name="mobile"
+                               type="email"
+                               autoComplete="mobile"
+                               value={form.mobile}
+                               onChange={SetData}
+                               required
                                className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                               placeholder="Enter your email" />
-                        <input id="email-address" name="email" type="email" autoComplete="email" required
+                               placeholder="Enter your mobile number" />
+
+                        <input id="password"
+                               name="password"
+                               type="password"
+                               autoComplete="password"
+                               value={form.password}
+                               onChange={SetData}
+                               required
                                className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                               placeholder="Enter your email" />
-                        <button type='button' onClick={LoginRequest}>
+                               placeholder="Enter your password" />
+
+                        <button type='button' onClick={submitHandler}>
                             login
                         </button>
                     </form>
