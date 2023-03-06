eaedba8ec940990b039a4144803094a68078e417
mehrdadshirvan <mysmartapply.it@gmail.com>
2023-03-06 20:23:20 +0330

    helper functions

---
+++ pages/helper.tsx
@@ -0,0 +1,4 @@
+export function baseUrl(url = ''){
+    // return 'https://new.visapickmap.com/api/panel/v1/user/' + (url!=''?'/'+url:'');
+    return 'https://rosena.ir/api' + (url!=''?'/'+url:'');
+}
